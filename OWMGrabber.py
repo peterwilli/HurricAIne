6ac4bef6c3518c4b779a51023223065ebdddfd60
Peter Willemsen <peter@codebuffet.co>
2017-10-14 16:48:07 +0200

    converted to py

--- OWMGrabber.py
+++ OWMGrabber.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import pyowm
@@ -13,13 +13,13 @@ from shutil import move
 from more_itertools import unique_everseen
 
 
-# In[ ]:
+# In[2]:
 
 
 csv_path = "HurricaneData/owm_houston.csv"
 
 
-# In[ ]:
+# In[3]:
 
 
 with open('config.yml') as f:
@@ -27,7 +27,7 @@ with open('config.yml') as f:
     config = yaml.safe_load(f)
 
 
-# In[ ]:
+# In[8]:
 
 
 while True:
@@ -35,25 +35,25 @@ while True:
     fc = owm.daily_forecast('Texas')
     f = fc.get_forecast()
     file_exists = os.path.isfile(csv_path)
-    headers = ['timestamp', 'temp', 'status', 'cloud_coverage', 'humidity', 'pressure', 'sea_level', 'rain', 'snow']
+    headers = ['timestamp', 'Max.TemperatureF', 'Min.TemperatureF', 'status_short', 'status', 'cloud_coverage', 'humidity', 'pressure', 'sea_level', 'rain', 'snow']
     with open(csv_path, 'a') as csvfile:
         writer = csv.writer(csvfile, delimiter=';',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         if not file_exists:
             writer.writerow(headers)
-
+        
         for w in f.get_weathers():
             rain = w.get_rain()
             snow = w.get_snow()
-            temp = w.get_temperature(unit='celsius')
+            temp = w.get_temperature(unit='fahrenheit')
             pres = w.get_pressure()
-            to_write = [w.get_reference_time(), temp.get('temp', ''), w.get_detailed_status(), w.get_clouds(), w.get_humidity(), pres.get('press', ''), pres.get('sea_level', ''), rain.get('all', ''), snow.get('all', '')]
+            to_write = [w.get_reference_time(), temp.get('max', ''), temp.get('min', ''), w.get_status(), w.get_detailed_status(), w.get_clouds(), w.get_humidity(), pres.get('press', ''), pres.get('sea_level', ''), rain.get('all', ''), snow.get('all', '')]
             writer.writerow(to_write)
-
+            
     with open(csv_path,'r') as f, open(csv_path + '.new','w') as out_file:
         out_file.writelines(unique_everseen(f))
         move(csv_path + '.new', csv_path)
-
-    print("Wrote weather lines")
-
+        
+    print('written lines')
     time.sleep(60 * 60)
+
